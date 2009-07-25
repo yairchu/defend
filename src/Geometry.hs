d29fdc51b20e67b7b8d81af1a82e497048f70de3
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2009-07-26 02:47:12 +0300

    correct triangulation calculated

--- src/Geometry.hs
+++ src/Geometry.hs
@@ -3,9 +3,6 @@ module Geometry (
   polygonEdges, triangulatePolygon
   ) where
 
-import Data.List (minimumBy)
-import Data.Ord (comparing)
-
 faceNormal :: (Floating a, Ord a) => [[a]] -> [a]
 faceNormal points =
   normalizeVector [a1*b2-a2*b1, a2*b0-a0*b2, a0*b1-a1*b0]
@@ -30,17 +27,22 @@ tailRot (x : xs) = xs ++ [x]
 
 type Line a = ((a, a), (a, a))
 
-lineIntersection :: Floating a => Line a -> Line a -> (a, a)
-lineIntersection a@((xa0, _), (xa1, _)) b@((xb0, yb0), (xb1, yb1))
-  | xa0 == xa1 =
-    (xa0, yb0 + (xa0-xb0) * (yb1-yb0) / (xb1-xb0))
-  | xb0 == xb1 = lineIntersection b a
-  | otherwise =
-    (x, yAt0 a + x * slope a)
-  where
-    x = (yAt0 a - yAt0 b) / (slope b - slope a)
-    yAt0 s@((x0, y0), _) = y0 - x0 * slope s
-    slope ((x0, y0), (x1, y1)) = (y1-y0)/(x1-x0)
+slope :: Fractional a => Line a -> Maybe a
+slope ((x0, y0), (x1, y1))
+  | x0 == x1 = Nothing
+  | otherwise = Just $ (y1-y0) / (x1-x0)
+
+lineIntersection :: Fractional a => Line a -> Line a -> (a, a)
+lineIntersection a@((xa0, _), _) b@((xb0, yb0), _) =
+  case (slope a, slope b) of
+    (Nothing, Just db) -> (xa0, yb0 + (xa0-xb0) * db)
+    (_, Nothing) -> lineIntersection b a
+    (Just da, Just db) ->
+      let
+        x = (yAt0 a da - yAt0 b db) / (db - da)
+        yAt0 ((x0, y0), _) d = y0 - x0 * d
+      in
+        (x, yAt0 a da + x * da)
 
 expandPolygon :: Floating a => a -> [(a, a)] -> [(a, a)]
 expandPolygon ammount outline =
@@ -54,17 +56,29 @@ expandPolygon ammount outline =
       where
         [nx, ny] = normalizeVector [ay - by, bx - ax]
 
-minimumOn :: Ord b => (a -> b) -> [a] -> a
-minimumOn func = minimumBy (comparing func)
+isFrontTriangle :: (Ord a, Num a) => [(a, a)] -> Bool
+isFrontTriangle points =
+  (x2-x0)*(y1-y0)-(x1-x0)*(y2-y0) >= 0
+  where
+    [(x0, y0), (x1, y1), (x2, y2)] = points
+
+pointInPolygon :: (Ord a, Fractional a) => (a, a) -> [(a, a)] -> Bool
+pointInPolygon point@(px, py) polygon =
+  length (filter crosses (polygonEdges polygon)) `mod` 2 == 1
+  where
+    horizLine = ((0, py), (1, py))
+    crosses e@((_, ey0), (_, ey1)) =
+      ix > px && iy > min ey0 ey1 && iy < max ey0 ey1
+      where
+        (ix, iy) = lineIntersection horizLine e
 
 triangulatePolygon :: (Ord a, Floating a) => [(a, a)] -> [[(a, a)]]
-triangulatePolygon xs
-  | length xs < 3 = []
-  | otherwise = cur : triangulatePolygon rest
+triangulatePolygon points
+  | length points < 3 = []
+  | not (isFrontTriangle abc) ||
+    any (`pointInPolygon` abc) rest =
+    triangulatePolygon (tailRot points)
+  | otherwise = abc : triangulatePolygon ([a, c] ++ rest)
   where
-    argMinX = minimumOn (fst . (xs !!)) [0 .. length xs - 1]
-    (pre, x : post) = splitAt argMinX xs
-    rest = post ++ pre
-    cur = [last rest, x, head rest]
-
+    (abc@[a, b, c], rest) = splitAt 3 points
 

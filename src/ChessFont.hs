8eae231c9d5075f73f6ddfa954003b637aa63c45
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2009-07-26 03:19:32 +0300

    font triangulation is memoized

--- src/ChessFont.hs
+++ src/ChessFont.hs
@@ -1,4 +1,4 @@
-module ChessFont (DrawPos, PiecePix(..), piecePix, pixBody) where
+module ChessFont (DrawPos, PiecePix(..), piecePix) where
 
 import Chess (PieceType(..))
 import Geometry (triangulatePolygon)
@@ -8,42 +8,37 @@ import Graphics.UI.GLUT (GLfloat)
 type DrawPos = (GLfloat, GLfloat)
 
 data PiecePix = PiecePix {
-  pixOutline :: [[DrawPos]]
+  pixOutline :: [[DrawPos]],
+  -- | pixBody can (and is) be calculated from pixOutline
+  -- it is in PiecePix for memoing this calculation.
+  pixBody :: [[DrawPos]]
 }
 
-calcBody :: [[DrawPos]] -> [[DrawPos]]
-calcBody = concatMap triangulatePolygon
-
-pixBody :: PiecePix -> [[DrawPos]]
-pixBody = calcBody . pixOutline
+mkPix :: [[DrawPos]] -> PiecePix
+mkPix outlines =
+  PiecePix {
+    pixOutline = outlines,
+    pixBody = outlines >>= triangulatePolygon
+  }
 
 piecePix :: PieceType -> PiecePix
 piecePix Pawn =
-  PiecePix r
+  mkPix [[(-s, -s), (-s, s), (s, s), (s, -s)]]
   where
-    r = [[(-s, -s), (-s, s), (s, s), (s, -s)]]
     s = 0.6
-piecePix Rook = PiecePix {
-  pixOutline = [thing ++ othing]
-  }
+piecePix Rook =
+  mkPix [thing ++ othing]
   where
     s = 0.5
     thing = [(-s, s), (-1, 1), (1, 1), (s, s)]
     othing = map r thing
     r (x, y) = (-x, -y)
-piecePix Knight = PiecePix {
-  pixOutline = [outline]
-  }
-  where
-    outline = [(-1, 0), (1, 1), (0.5, -1), (-1, -1), (0, 0)]
-piecePix Bishop = PiecePix {
-  pixOutline = [outline]
-  }
-  where
-    outline = [(-1, -1), (0, 1), (1, -1), (0, -0.5)]
-piecePix King = PiecePix {
-  pixOutline = [reverse leye, reye, mouthline]
-  }
+piecePix Knight =
+  mkPix [[(-1, 0), (1, 1), (0.5, -1), (-1, -1), (0, 0)]]
+piecePix Bishop =
+  mkPix [[(-1, -1), (0, 1), (1, -1), (0, -0.5)]]
+piecePix King =
+  mkPix [reverse leye, reye, mouthline]
   where
     reye = [(0.25, 0.5), (0.5, 0.75), (0.75, 0.5), (0.5, 0.25)]
     leye = map m reye
@@ -52,12 +47,8 @@ piecePix King = PiecePix {
       [(-0.75, -0.75), (-0.75, -0.25)
       ,(0, -0.5)
       ,(0.75, -0.25), (0.75, -0.75)]
-piecePix Queen = PiecePix {
-  pixOutline = [outline]
-  }
-  where
-    outline =
-      [(-1, 1), (-0.25, 0.5), (0, 1), (0.25, 0.5), (1, 1)
-      ,(0.5, 0), (1, -1), (0, -0.5), (-1, -1), (-0.5, 0)]
-
+piecePix Queen =
+  mkPix [
+    [(-1, 1), (-0.25, 0.5), (0, 1), (0.25, 0.5), (1, 1)
+    ,(0.5, 0), (1, -1), (0, -0.5), (-1, -1), (-0.5, 0)]]
 

16121db175620755f48bdb0fc2a832ac771a8669
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2009-08-11 22:10:08 +0300

    simplified matching slightly

--- server/match.hs
+++ server/match.hs
@@ -2,7 +2,7 @@ import System.Environment
 
 main :: IO ()
 main = do
-  putStrLn . ("Content-type: text/plain\r\n\r\n" ++) =<< readFile hostFn
+  putStr . ("Content-type: text/plain\r\n\r\n" ++) =<< readFile hostFn
   writeFile hostFn =<< getEnv "QUERY_STRING"
   where
     hostFn = "/home/public/host.txt"

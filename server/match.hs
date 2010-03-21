07d4e6a5e341d09a1ccb4f0f05dae783f3384af4
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2010-03-21 02:39:37 +0200

    server support for conflicting client versions

--- server/match.hs
+++ server/match.hs
@@ -2,8 +2,13 @@ import System.Environment
 
 main :: IO ()
 main = do
-  putStr . ("Content-type: text/plain\r\n\r\n" ++) =<< readFile hostFn
-  writeFile hostFn =<< getEnv "QUERY_STRING"
-  where
-    hostFn = "/home/public/host.txt"
-
+  query <- getEnv "QUERY_STRING"
+  let
+    hostDir = "/home/public/"
+    bchar = '-'
+    (gameVer, _:addrs) = break (== bchar) query
+    (filename, addrsString)
+      | bchar `elem` query = (hostDir ++ "host_" ++ gameVer ++ ".txt", addrs)
+      | otherwise = (hostDir ++ "host.txt", query)
+  putStr . ("Content-type: text/plain\r\n\r\n" ++) =<< readFile filename
+  writeFile filename addrsString
